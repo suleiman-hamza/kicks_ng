dfe6ff8ba9f1a3b4404f9f8ab31c4fea48b7dd85
suleiman hamza <hamzasuleiman530@gmail.com>
2025-03-05 10:57:40 +0100

    remove Cataview route from the router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,9 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
-import Cataview from '@/views/Cataview.vue';
 import Main from '@/views/Main.vue';
 import FavouriteView from '@/views/FavouriteView.vue';
 
 const routes = [
     { path: '/', component: Main },
-    { path: '/about', component: Cataview },
     { path: '/fave', component: FavouriteView }
   ]
   
